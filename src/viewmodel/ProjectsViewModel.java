5b2935f499f3a634938f5666bececb70ee56d64b
Cosmin Demian <cosmindemian49@gmail.com>
2023-04-17 12:26:18 +0200

    ViewModel ProjectsViewModel

--- src/viewmodel/ProjectsViewModel.java
+++ src/viewmodel/ProjectsViewModel.java
@@ -2,13 +2,14 @@ package viewmodel;
 
 import javafx.beans.property.*;
 import model.Employee;
+import model.Model;
 import model.Project;
 import model.Task;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class ProjectsViewModel extends ViewModel
+public class ProjectsViewModel implements ViewModel
 {
     private StringProperty titleProperty;
     private StringProperty descriptionProperty;
@@ -19,11 +20,17 @@ public class ProjectsViewModel extends ViewModel
     private ArrayList<Task> projectTasks;
     private StringProperty error;
 
-    public ProjectsViewModel(Project project) {
-        super();
-        titleProperty = new SimpleStringProperty(project.getTitle());
-        descriptionProperty = new SimpleStringProperty(project.getDescription());
-        deadlineProperty = new SimpleObjectProperty<>(project.getDeadline());
+    public ProjectsViewModel(Model model) {
+        this.error = new SimpleStringProperty();
+        this.projectTasks = new ArrayList<>();
+        this.employees= new ArrayList<>();
+        this.projectManagerProperty=new SimpleStringProperty();
+        this.deadlineProperty=new SimpleObjectProperty<>();
+        this.descriptionProperty=new SimpleStringProperty();
+        this.titleProperty=new SimpleStringProperty();
+        //titleProperty = new SimpleStringProperty(project.getTitle());
+        //descriptionProperty = new SimpleStringProperty(project.getDescription());
+        //deadlineProperty = new SimpleObjectProperty<>(project.getDeadline());
         //projectManagerProperty = new SimpleStringProperty(project.getProjectManager().getFirstName() + " " + project.getProjectManager().getLastName() + " " + project.getProjectManager().getPhoneNumber());
     }
 
