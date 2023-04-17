717deec57b65d87861db0679511ac3f23f7f23a9
artur <janiszale112233@gmail.com>
2023-04-17 11:02:46 +0200

    maybe projectManagerProperty should be updated

---
+++ src/viewmodel/ProjectViewModel.java
@@ -0,0 +1,61 @@
+package viewmodel;
+
+import javafx.beans.property.*;
+import model.Employee;
+import model.Project;
+import model.Task;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class ProjectViewModel extends ViewModel
+{
+    private StringProperty titleProperty;
+    private StringProperty descriptionProperty;
+    private ObjectProperty<LocalDate> deadlineProperty;
+    private StringProperty projectManagerProperty;
+
+    private ArrayList<Employee> employees;
+    private ArrayList<Task> projectTasks;
+    private StringProperty error;
+
+    public ProjectViewModel(Project project) {
+        super();
+        titleProperty = new SimpleStringProperty(project.getTitle());
+        descriptionProperty = new SimpleStringProperty(project.getDescription());
+        deadlineProperty = new SimpleObjectProperty<>(project.getDeadline());
+        projectManagerProperty = new SimpleStringProperty(project.getProjectManager().getFirstName() + " " + project.getProjectManager().getLastName() + " " + project.getProjectManager().getPhoneNumber());
+    }
+
+
+    public StringProperty titleProperty() {
+        return titleProperty;
+    }
+
+    public StringProperty descriptionProperty() {
+        return descriptionProperty;
+    }
+
+    public LocalDate getDeadlineProperty() {
+        return deadlineProperty.get();
+    }
+
+    public StringProperty projectManagerProperty() {
+        return projectManagerProperty;
+    }
+
+    public ObjectProperty<LocalDate> deadlineProperty() {
+        return deadlineProperty;
+    }
+
+    public void setDeadlineProperty(LocalDate deadlineProperty) {
+        this.deadlineProperty.set(deadlineProperty);
+    }
+
+    public ArrayList<Employee> getEmployeesWithAccess() {
+        Project project = null;
+        return new ArrayList<>(project.getEmployeesWithAccess());
+    }
+
+}
