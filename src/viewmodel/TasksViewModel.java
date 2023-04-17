9fc7c1bcd5db28e864cf36eca26d3676f40e507a
Reblayzer <104923443+Reblayzer@users.noreply.github.com>
2023-04-17 11:10:16 +0200

    adding taskViewModel

---
+++ src/viewmodel/TasksViewModel.java
@@ -0,0 +1,34 @@
+package viewmodel;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Model;
+import model.Task;
+import model.TaskList;
+
+import java.util.ArrayList;
+
+public class TasksViewModel extends ViewModel
+{
+    private Model model;
+    private TaskList taskList;
+    private ObservableList<Task> tasks;
+    private StringProperty error;
+    
+    public TasksViewModel(Model model)
+    {
+       this.error = new SimpleStringProperty();
+       this.model = model;
+       this.tasks = FXCollections.observableArrayList();
+       this.taskList = new TaskList();
+    }
+    
+    public StringProperty getError()
+    {
+        return error;
+    }
+    
+    
+}
