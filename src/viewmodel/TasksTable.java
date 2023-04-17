eda44952edd1405f6afcc2a9dca6e88882f7bf01
Reblayzer <104923443+Reblayzer@users.noreply.github.com>
2023-04-17 14:14:50 +0200

    Tables

---
+++ src/viewmodel/TasksTable.java
@@ -0,0 +1,76 @@
+package viewmodel;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.time.LocalDate;
+
+public class TasksTable
+{
+    private StringProperty title;
+    private ObjectProperty<LocalDate> deadline;
+    private StringProperty priority;
+    private StringProperty status;
+    private StringProperty tags;
+    
+    public TasksTable(String title, LocalDate deadline, String priority, String status, String tags)
+    {
+    this.title = new SimpleStringProperty(title);
+    this.deadline = new SimpleObjectProperty<>();
+    this.priority = new SimpleStringProperty(priority);
+    this.status = new SimpleStringProperty(status);
+    this.tags = new SimpleStringProperty(tags);
+    }
+    
+    public String getTitle()
+    {
+        return title.get();
+    }
+    
+    public StringProperty titleProperty()
+    {
+        return title;
+    }
+    
+    public LocalDate getDeadline()
+    {
+        return deadline.get();
+    }
+    
+    public ObjectProperty<LocalDate> deadlineProperty()
+    {
+        return deadline;
+    }
+    
+    public String getPriority()
+    {
+        return priority.get();
+    }
+    
+    public StringProperty priorityProperty()
+    {
+        return priority;
+    }
+    
+    public String getStatus()
+    {
+        return status.get();
+    }
+    
+    public StringProperty statusProperty()
+    {
+        return status;
+    }
+    
+    public String getTags()
+    {
+        return tags.get();
+    }
+    
+    public StringProperty tagsProperty()
+    {
+        return tags;
+    }
+}
--- src/viewmodel/TasksViewModel.java
+++ src/viewmodel/TasksViewModel.java
@@ -1,18 +1,28 @@
 package viewmodel;
 
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import model.Employee;
 import model.Model;
 import model.Task;
 import model.TaskList;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class TasksViewModel implements ViewModel
 {
     private Model model;
+    private StringProperty projectNameProperty;
+    private StringProperty taskNameProperty;
+    private StringProperty taskDescriptionProperty;
+    
+    
+    
+    private ArrayList<Employee> employees;
     private TaskList taskList;
     private ObservableList<Task> tasks;
     private StringProperty error;
@@ -30,5 +40,13 @@ public class TasksViewModel implements ViewModel
         return error;
     }
     
+    public TaskList getTaskList()
+    {
+        return taskList;
+    }
     
+    public ObservableList<Task> getTasks()
+    {
+        return tasks;
+    }
 }
