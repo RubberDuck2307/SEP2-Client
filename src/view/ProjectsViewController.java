d289919263fac4ec92def14cecaf6bba6f491bcb
Jan Metela <janmetela@seznam.cz>
2023-04-26 16:19:42 +0200

    fixing small error

--- src/view/ProjectsViewController.java
+++ src/view/ProjectsViewController.java
@@ -11,45 +11,50 @@ import viewmodel.ViewModel;
 
 import java.time.LocalDate;
 
-public class ProjectsViewController implements ViewController
-{
-  public TableColumn<ProjectsViewModel, Void> delete;
-  public TableColumn edit;
-  @FXML
-  private TableView<ProjectsTable> projectTable;
-  @FXML
-  private TableView<ProjectsTable> employeesListTable;
+public class ProjectsViewController implements ViewController {
+    public TableColumn<ProjectsViewModel, Void> delete;
+    public TableColumn edit;
+    @FXML
+    private TableView<ProjectsTable> projectTable;
+    @FXML
+    private TableView<ProjectsTable> employeesListTable;
 
-  @FXML private TableColumn<ProjectsTable, String>  titleColumn;
-  @FXML private TableColumn<ProjectsTable, String> deadlineColumn;
-  @FXML private TableColumn<ProjectsTable, String> managerColumn;
-  @FXML private TableColumn<WorkersTable, String>  projectEmployeeNameColumn;
-  @FXML private TableColumn<WorkersTable, String> projectEmployeePositionColumn;
-  @FXML private TextArea descriptionArea;
-  @FXML private Label titleLabel;
+    @FXML
+    private TableColumn<ProjectsTable, String> titleColumn;
+    @FXML
+    private TableColumn<ProjectsTable, String> deadlineColumn;
+    @FXML
+    private TableColumn<ProjectsTable, String> managerColumn;
+    @FXML
+    private TableColumn<WorkersTable, String> projectEmployeeNameColumn;
+    @FXML
+    private TableColumn<WorkersTable, String> projectEmployeePositionColumn;
+    @FXML
+    private TextArea descriptionArea;
+    @FXML
+    private Label titleLabel;
 
-  private Region root;
-  private ProjectsViewModel viewModel;
-  private ViewHandler viewHandler;
+    private Region root;
+    private ProjectsViewModel viewModel;
+    private ViewHandler viewHandler;
 
-  @Override public void init(ViewHandler viewHandler, ViewModel viewModel,
-                             Region root)
-  {
-    this.root = root;
-    this.viewHandler = viewHandler;
-    this.viewModel = (ProjectsViewModel) viewModel;
+    @Override
+    public void init(ViewHandler viewHandler, ViewModel viewModel,
+                     Region root) {
+        this.root = root;
+        this.viewHandler = viewHandler;
+        this.viewModel = (ProjectsViewModel) viewModel;
 
-    titleLabel.textProperty().bindBidirectional(this.viewModel.getTitleProperty());
-    descriptionArea.textProperty().bindBidirectional(this.viewModel.getDescriptionProperty());
+        titleLabel.textProperty().bindBidirectional(this.viewModel.getTitleProperty());
+        descriptionArea.textProperty().bindBidirectional(this.viewModel.getDescriptionProperty());
 
 
-    titleColumn.setCellValueFactory(cellData -> cellData.getValue().getTitleValue());
-    deadlineColumn.setCellValueFactory(cellData -> cellData.getValue().deadlineProperty());
-    managerColumn.setCellValueFactory(cellData -> cellData.getValue().getManagerValue());
+        titleColumn.setCellValueFactory(cellData -> cellData.getValue().getTitleValue());
+        deadlineColumn.setCellValueFactory(cellData -> cellData.getValue().deadlineProperty());
+        managerColumn.setCellValueFactory(cellData -> cellData.getValue().getManagerValue());
 
-    projectEmployeeNameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
-    projectTable.setItems(this.viewModel.getProjectsObservableList());
-
+        projectEmployeeNameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        projectTable.setItems(this.viewModel.getProjectsObservableList());
 
 
 //    delete.setCellFactory(column -> {
@@ -80,20 +85,22 @@ public class ProjectsViewController implements ViewController
 //
 //
 //    //projectsListTable.setItems();
-  }
+    }
 
-  @Override public void reset()
-  {
+    @Override
+    public void reset() {
 
-  }
+    }
 
-  @FXML public void projectTableClick(){
-    viewModel.setProject(projectTable.getSelectionModel().getSelectedItem().getId());
-    viewHandler.openView("tasks");
-  }
+    @FXML
+    public void projectTableClick() {
+        if (projectTable.getSelectionModel().getSelectedItem() != null) {
+            viewModel.setProject(projectTable.getSelectionModel().getSelectedItem().getId());
+            viewHandler.openView("tasks");
+        }
+    }
 
-  public Region getRoot()
-  {
-    return root;
-  }
+    public Region getRoot() {
+        return root;
+    }
 }
