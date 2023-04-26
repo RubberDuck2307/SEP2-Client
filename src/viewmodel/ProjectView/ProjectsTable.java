061307588b7feb7c7efd044934dd01bc4c200f16
AnnaAndrlova <andrlovaanicka@seznam.cz>
2023-04-26 10:09:28 +0200

    Projects table

--- src/viewmodel/ProjectView/ProjectsTable.java
+++ src/viewmodel/ProjectView/ProjectsTable.java
@@ -1,9 +1,11 @@
 package viewmodel.ProjectView;
 
+import javafx.beans.Observable;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
 
 import java.time.LocalDate;
 
@@ -12,7 +14,6 @@ public class ProjectsTable
   private StringProperty title;
   private ObjectProperty<LocalDate> deadline;
   private StringProperty manager;
-
 
   public ProjectsTable(String title, LocalDate deadline, String manager)
   {
@@ -47,6 +48,20 @@ public class ProjectsTable
   }
 
   public StringProperty managerProperty()
+  {
+    return manager;
+  }
+
+  public ObservableValue<String> getTitleValue()
+
+  {
+    return title;
+  }public ObservableValue<LocalDate> getDeadlineValue()
+
+  {
+    return deadline;
+  }public ObservableValue<String> getManagerValue()
+
   {
     return manager;
   }
