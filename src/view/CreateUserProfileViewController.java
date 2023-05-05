ad67eca960fb1bfe3555fb7b790ae153a3317df3
Reblayzer <104923443+Reblayzer@users.noreply.github.com>
2023-05-05 10:45:24 +0200

    create user profile

--- src/view/CreateUserProfileViewController.java
+++ src/view/CreateUserProfileViewController.java
@@ -61,27 +61,27 @@ public class CreateUserProfileViewController implements ViewController
         this.root = root;
         System.out.println(root);
         this.viewHandler = viewHandler;
-//        this.viewModel = (CreateUserProfileViewModel) viewModel;
-//        setChoiceBox();
-//        this.dobErrorL.setText(null);
-//        this.emailErrorL.setText(null);
-//        this.lastNameErrorL.setText(null);
-//        this.firstNameErrorL.setText(null);
-//        this.roleErrorL.setText(null);
-//        this.phoneNumberErrorL.setText(null);
-//        this.passwordErrorL.setText(null);
-//        firstNameL.textProperty().bindBidirectional(this.viewModel.firstNameProperty());
-//        firstNameErrorL.textProperty().bindBidirectional(this.viewModel.firstNameEProperty());
-//        lastNameL.textProperty().bindBidirectional(this.viewModel.lastNameProperty());
-//        lastNameErrorL.textProperty().bindBidirectional(this.viewModel.lastNameEProperty());
-//        emailL.textProperty().bindBidirectional(this.viewModel.emailProperty());
-//        emailErrorL.textProperty().bindBidirectional(this.viewModel.emailEProperty());
-//        roleErrorL.textProperty().bindBidirectional(this.viewModel.roleEProperty());
-//        phoneNumberL.textProperty().bindBidirectional(this.viewModel.phoneNumberProperty());
-//        phoneNumberErrorL.textProperty().bindBidirectional(this.viewModel.phoneNumberEProperty());
-//        passwordL.textProperty().bindBidirectional(this.viewModel.passwordProperty());
-//        passwordErrorL.textProperty().bindBidirectional(this.viewModel.passwordEProperty());
-//        roleL.setItems(this.viewModel.roleProperty());
+        this.viewModel = (CreateUserProfileViewModel) viewModel;
+        setChoiceBox();
+        this.dobErrorL.setText(null);
+        this.emailErrorL.setText(null);
+        this.lastNameErrorL.setText(null);
+        this.firstNameErrorL.setText(null);
+        this.roleErrorL.setText(null);
+        this.phoneNumberErrorL.setText(null);
+        this.passwordErrorL.setText(null);
+        firstNameL.textProperty().bindBidirectional(this.viewModel.firstNameProperty());
+        firstNameErrorL.textProperty().bindBidirectional(this.viewModel.firstNameEProperty());
+        lastNameL.textProperty().bindBidirectional(this.viewModel.lastNameProperty());
+        lastNameErrorL.textProperty().bindBidirectional(this.viewModel.lastNameEProperty());
+        emailL.textProperty().bindBidirectional(this.viewModel.emailProperty());
+        emailErrorL.textProperty().bindBidirectional(this.viewModel.emailEProperty());
+        roleErrorL.textProperty().bindBidirectional(this.viewModel.roleEProperty());
+        phoneNumberL.textProperty().bindBidirectional(this.viewModel.phoneNumberProperty());
+        phoneNumberErrorL.textProperty().bindBidirectional(this.viewModel.phoneNumberEProperty());
+        passwordL.textProperty().bindBidirectional(this.viewModel.passwordProperty());
+        passwordErrorL.textProperty().bindBidirectional(this.viewModel.passwordEProperty());
+        roleL.setItems(this.viewModel.roleProperty());
     }
     
     @Override
