8d077d8f5d6b2fbaf3f20a824e3dcd7c902b6e2d
Jan Metela <janmetela@seznam.cz>
2023-05-22 12:22:06 +0200

    writing javadoc

--- src/util/Validator.java
+++ src/util/Validator.java
@@ -12,8 +12,15 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * A class for validating user input.
+ */
 public class Validator
 {
+    /**
+     * checks if the firstname is not an empty string or a string with only one character. If it is it throws an exception.
+     * @param firstName
+     */
     public void validateFirstName(String firstName)
     {
         if (firstName.trim().isEmpty())
@@ -25,6 +32,11 @@ public class Validator
             throw new RuntimeException("First name has to be longer than one character!");
         }
     }
+
+    /**
+     * checks if the lastname is not an empty string or a string with only one character. If it is it throws an exception.
+     * @param lastName
+     */
     
     public void validateLastName(String lastName)
     {
@@ -37,7 +49,11 @@ public class Validator
             throw new RuntimeException("Last name has to be longer than one character!");
         }
     }
-    
+
+    /**
+     * checks if the name is not an empty string or a string with only one character. If it is it throws an exception.
+     * @param name
+     */
     public void validateName(String name)
     {
         if (name.trim().isEmpty())
@@ -49,7 +65,11 @@ public class Validator
             throw new RuntimeException("Name has to be longer than one character!");
         }
     }
-    
+
+    /**
+     * checks if the email is not an empty string and if it is in valid format.If it is it throws an exception.
+     * @param emailAddress
+     */
     public void validateEmail(String emailAddress)
     {
         String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
@@ -66,7 +86,12 @@ public class Validator
             throw new RuntimeException("Wrong email format!");
         }
     }
-    
+
+
+    /**
+     * checks if the phone number is not an empty string and if it is in valid format.If it is it throws an exception.
+     * @param phoneNumber
+     */
     public void validatePhoneNumber(String phoneNumber)
     {
         String patterns = "^\\+?\\d{1,20}$";
@@ -90,7 +115,9 @@ public class Validator
             System.out.println(removedPlus.getValue());
         }
     }
-    
+    /**
+     * checks if the password is not an empty string and if it is longer than 4 characters.If it is it throws an exception.
+     */
     public void validatePassword(String password)
     {
         if (password.trim().isEmpty())
@@ -102,7 +129,11 @@ public class Validator
             throw new RuntimeException("Password mast be longer than 4 characters");
         }
     }
-    
+
+    /**
+     * checks if the date of birth is in the past. If it is not throws an exception.
+     * @param date
+     */
     public void validateDOB(LocalDate date)
     {
         if (!date.isBefore(LocalDate.now()))
@@ -110,7 +141,11 @@ public class Validator
             throw new RuntimeException("Date of birth has to be in the past");
         }
     }
-    
+
+    /**
+     * checks if the deadline of the project is in the future. If it is not throws an exception.
+     * @param deadline
+     */
     public void validateDeadline(LocalDate deadline)
     {
         if (deadline!=null && !deadline.isAfter(LocalDate.now()))
@@ -118,6 +153,12 @@ public class Validator
             throw new RuntimeException("Deadline has to be in the future");
         }
     }
+
+    /**
+     * checks if the deadline of the task is not after the deadline of the project. If it is throws an exception.
+     * @param deadlineOfTheTask
+     * @param deadlineOfTheProject
+     */
     public void validateTaskDeadline(LocalDate deadlineOfTheTask, LocalDate deadlineOfTheProject)
     {
         if (deadlineOfTheTask.isAfter(deadlineOfTheProject))
@@ -125,7 +166,12 @@ public class Validator
             throw new RuntimeException("Deadline of the task can not be after the deadline of the project");
         }
     }
-    
+
+
+    /**
+     * checks if the choice box is not empty. If it is throws an exception.
+     * @param choiceBox
+     */
     public void validateChoiceBox(String choiceBox)
     {
         if (choiceBox.trim().isEmpty())
@@ -133,7 +179,11 @@ public class Validator
             throw new RuntimeException("Choice box cannot be empty!");
         }
     }
-    
+
+    /**
+     * checks if the title is not an empty string or a string with only one character. If it is throws an exception.
+     * @param title
+     */
     public void validateTitle(String title)
     {
         if (title==null || title.trim().isEmpty())
@@ -145,7 +195,11 @@ public class Validator
             throw new RuntimeException("Title has to be longer than one character!");
         }
     }
-    
+
+    /**
+     * checks if the estimated time does not contain letters. If it does, throws an exception.
+     * @param estimatedTime
+     */
     public void validateEstimatedTimer(String estimatedTime)
     {
         String patterns = "^( )?[0-9]*$";
@@ -159,19 +213,23 @@ public class Validator
         }
     }
 
+    /**
+     * check if the color is null. If it is throws an exception.
+     * @param color
+     */
     public void validateColor(Color color)
     {
         if (color==null)
         {
             throw new RuntimeException("Color cannot be empty!");
         }
-//        if(color.getBrightness()<0.5){
-//            throw new RuntimeException("Color cannot be too dark!");
-//        }
-//        if(color.getSaturation()<0.3){
-//            throw new RuntimeException("Color cannot be too pale!");
-//        }
+
     }
+
+    /**
+     * checks if name or color of the tag are null. If they are throws an exception.
+     * @param tag
+     */
 
     public void validateTag(Tag tag)
     {
